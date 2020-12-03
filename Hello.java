c962839f4b6ce2720d262c6cc66cc46dbcb71b6c
supriyashevate <supriyashevate17@gmail.com>
2020-12-03 22:21:50 +0530

    Updated hello world program

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
-
 public class Hello{
    public static void main(String[] args){
      for(int i=1;i<=10;i++)
      {
-        System.out.println("Hello Supriya, Welcome to Jenkins ..."+i);
+        System.out.println("Hello Akash, Welcome to Jenkins ..."+i);
      }
    }
 }
